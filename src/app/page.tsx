d9298ec89338fd2519618bd26ee96d5e8425c974
Muhammad-Hussain-Ullah <160456309+Muhammad-Hussain-Ullah@users.noreply.github.com>
2024-09-28 21:24:42 +0500

    Now everything is completed for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="intro">
           <h1>Hi,</h1>
           <div className="aboutme">I am <span className="name">Hussain</span></div>
-          <span className="role">I am Hussain, a college student living in Karachi, Pakistan. Currently, I'm in my second year of studies, pursuing a web development course at GIAIC to sharpen my skills for the digital age. In my free time, I love immersing myself in the vibrant world of anime, which offers me both entertainment and inspiration. Focused on my goals, I'm steadily building my future while balancing my passion for learning with my love for leisure.</span>
+          <span className="role">A college student living in Karachi, Pakistan. Currently, I'm in my second year of studies, pursuing a web development course at GIAIC to sharpen my skills for the digital age. In my free time, I love immersing myself in the vibrant world of anime, which offers me both entertainment and inspiration. Focused on my goals, I'm steadily building my future while balancing my passion for learning with my love for leisure.</span>
         </div>
         <div>
           <img src="my pfp.jpg" alt="My image" className="my-image"/>
